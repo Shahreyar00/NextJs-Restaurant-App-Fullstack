59ccf2f9c6f739d8da73a0ccedaa43ee9493f915
Shahreyar Arif <70688937+Shahreyar00@users.noreply.github.com>
2024-01-27 22:08:04 +0530

    chore:added delete functionality for single product if you are an admin

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,4 @@
+import DeleteButton from '@/components/DeleteButton';
 import Price from '@/components/Price'
 // import { singleProduct } from '@/data'
 import { ProductType } from '@/types/types';
@@ -32,7 +33,10 @@ const SingleProductPage = async ({ params }: { params: { id: string } }) => {
                 </div>
             )}
             <div className="h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
-                <h1 className="text-3xl font-bold uppercase xl:text-5xl">{singleProduct.title}</h1>
+                <h1 className="text-3xl font-bold uppercase">
+                    <span>{singleProduct.title}</span>
+                    <DeleteButton id={singleProduct.id} />
+                </h1>
                 <p>{singleProduct.desc}</p>
                 <Price product={singleProduct} />
             </div>
