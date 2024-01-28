12ff8c8c755b6c91c27dd26fa20664fd5bf6012b
Shahreyar Arif <70688937+Shahreyar00@users.noreply.github.com>
2024-01-28 21:59:35 +0530

    added routes for handling payment

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -65,7 +65,7 @@ const CartPage = () => {
             <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-10 2xl:text-xl 2xl:gap-6">
                 <div className="flex justify-between">
                     <span className="">Subtotal ({totalItems} items)</span>
-                    <span className="">${totalPrice}</span>
+                    <span className="">${totalPrice.toFixed(2)}</span>
                 </div>
                 <div className="flex justify-between">
                     <span className="">Service Cost</span>
@@ -78,7 +78,7 @@ const CartPage = () => {
                 <hr className="my-2" />
                 <div className="flex justify-between">
                     <span className="">TOTAL(INCL. VAT)</span>
-                    <span className="font-bold">${totalPrice}</span>
+                    <span className="font-bold">${totalPrice.toFixed(2)}</span>
                 </div>
                 <button
                     className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end"
--- src/components/AddressForm.tsx
+++ src/components/AddressForm.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { AddressElement } from "@stripe/react-stripe-js";
+import React from "react";
 
 const AddressForm = () => {
     return (
-        <div>AddressForm</div>
-    )
-}
+        <form>
+            <h3>Address</h3>
+            <AddressElement
+                options={{ mode: "shipping" }}
+                onChange={(event) => {
+                    if (event.complete) {
+                        const address = event.value.address;
+                    }
+                }}
+            />
+        </form>
+    );
+};
 
-export default AddressForm
+export default AddressForm;
